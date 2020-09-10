7087e5f702d964e305087b0b3cc8e9aba3da4b94
scottcm73 <scottcmcmahan@gmail.com>
2020-09-10 14:28:31 -0500

    Creates table with pandas and sqlalchemy creat_engine.

--- database_create.py
+++ database_create.py
@@ -1,12 +1,45 @@
 import pandas as pd
 import numpy as np
 import pymysql
+import os
+from sqlalchemy import create_engine
 from secret import USER, PASSWORD,  HOST, PORT, DATABASE, DIALECT, DRIVER, CODEWORD
+
+
+# This creates a single-table database and uses information from secret.py in connection string.
+# secret.py and secret2.py are ignored in GitHub.
  
-SQALLCHEMY_DATABASE_URI = f"{DIALECT}+{DRIVER}://{USER}:{PASSWORD}@{HOST}/{DATABASE}"
+SQALCHEMY_DATABASE_URI = f"{DIALECT}+{DRIVER}://{USER}:{PASSWORD}@{HOST}/{DATABASE}"
 
 engine = create_engine(
     SQALCHEMY_DATABASE_URI
 )
 
+mypath=os.path.join("resources", "players_stats_by_season_full_details.csv")
+tableName = "b_ball_player_stats"
+df = pd.read_csv(mypath)           
+dbConnection    = engine.connect()
 
+try:
+
+    frame = df.to_sql(tableName, dbConnection, if_exists='fail');
+
+except ValueError as vx:
+
+    print(vx)
+
+except Exception as ex:   
+
+    print(ex)
+
+else:
+
+    print("Table %s created successfully."%tableName);   
+
+finally:
+
+    dbConnection.close()
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()

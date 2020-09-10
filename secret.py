25eb83290a01181f84077c90c06d6204c1a4b7b8
scottcm73 <scottcmcmahan@gmail.com>
2020-09-10 11:19:17 -0500

    Added secret and secret2

---
+++ secret.py
@@ -0,0 +1,8 @@
+DIALECT = "mysql"
+DRIVER = "pymysql"
+USER = 'datascm2_scott'
+PASSWORD =  'Fk45QbHgy8GEMLQX8FAy5SHA2evXYwbMbd43xmzsJm92RE5GNDkRP7mQKPg2YDpA'
+HOST = '162.241.193.35'
+PORT='3306'
+DATABASE = 'datascm2_basketball_stats'
+CODEWORD = 'joedirt'
---
+++ secret2.py
@@ -0,0 +1,8 @@
+DIALECT = "mysql"
+DRIVER = "pymysql"
+USER = 'scott'
+PASSWORD =  'ynzkS6pV3nk76K3Jt82b1ux9UfUDhGQmLMZG8U7qo6nPNTfAA26U4QeSLOLKA1oh'
+HOST = '162.241.193.35'
+PORT='3306'
+DATABASE = 'basketballstats'
+CODEWORD = 'joedirt'

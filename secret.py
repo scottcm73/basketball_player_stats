4aa1b10f0bdfc82cc3b6d7667bb115c13f790bc6
scottcm73 <scottcmcmahan@gmail.com>
2020-09-10 14:29:01 -0500

    Updated to local host

--- secret.py
+++ secret.py
@@ -1,8 +1,8 @@
 DIALECT = "mysql"
 DRIVER = "pymysql"
-USER = 'datascm2_scott'
+USER = 'root'
 PASSWORD =  'Fk45QbHgy8GEMLQX8FAy5SHA2evXYwbMbd43xmzsJm92RE5GNDkRP7mQKPg2YDpA'
-HOST = '162.241.193.35'
+HOST = '127.0.0.1'
 PORT='3306'
-DATABASE = 'datascm2_basketball_stats'
+DATABASE = 'basketballstats'
 CODEWORD = 'joedirt'

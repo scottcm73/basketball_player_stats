8661a2c125671a43e77346127422c038c037f783
scottcm73 <scottcmcmahan@gmail.com>
2020-09-15 12:14:15 -0500

    Added app.py for flask app

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, json, jsonify, request
+import getdata
+from getdata import make_dict
+
+app = Flask(__name__)
+
+@app.route('/data')
+
+def hello_world():
+    the_dict=make_dict()
+    
+    print(the_dict)
+    return jsonify(the_dict)
+
+@app.route('/')
+
+def hello_world2():
+    
+    
+    return render_template("main.html" )
+
+app.run(debug=True)
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()

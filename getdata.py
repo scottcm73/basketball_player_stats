24dadc2d9609354ce5f1ec9d0f879133402b5510
scottcm73 <scottcmcmahan@gmail.com>
2020-09-17 13:04:51 -0500

    Query gets just NBA players

--- getdata.py
+++ getdata.py
@@ -56,7 +56,7 @@ class PlayerStats(Base):
 
 
 def make_dict():
-    all_player_stats=session.query(PlayerStats).all()
+    all_player_stats=session.query(PlayerStats).filter(PlayerStats.league=="NBA").all()
     the_dict=DictMixIn
     this_dict=[]
     for player_stats in all_player_stats:

084688e5c7731094d9c72246e0196c52b12b0226
Scott McMahan <53838044+scottcm73@users.noreply.github.com>
2020-10-01 15:05:29 -0500

    Delete secret.py
    Delete for deployment.

--- secret.py
+++
@@ -1,8 +0,0 @@
-DIALECT = "mysql"
-DRIVER = "pymysql"
-USER = 'root'
-PASSWORD =  'Fk45QbHgy8GEMLQX8FAy5SHA2evXYwbMbd43xmzsJm92RE5GNDkRP7mQKPg2YDpA'
-HOST = '127.0.0.1'
-PORT='3306'
-DATABASE = 'basketballstats'
-CODEWORD = 'joedirt'

f7127c7cc1d04319a88e826f07189510b6c3dc66
scottcm73 <scottcmcmahan@gmail.com>
2020-10-04 09:53:36 -0500

    Added host in app.run command

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+# This app uses a combination of Flask, MySQL, D3, and plotly.js to render graphs
+# based on basketball player statistics. 
+# First the data is retrieved from a MySQL database and jsonified. Then, jinga2, HTML and 
+# JavaScript are used to serve the main.html template page with graph rendering functionality.
 from flask import Flask, render_template, json, jsonify, request
 import getdata
 from getdata import make_dict
